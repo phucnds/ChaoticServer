b6c781a3e896d23ce18acb859d08fac360f7dce2
phucnds <42528099+phucnds@users.noreply.github.com>
2023-02-19 14:48:37 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const controller = require("../controller/file.controller");
 
 let routes = (app) => {
   router.get("/ping", controller.ping)
-  router.get("/pings", controller.ping)
   router.post("/upload", controller.upload);
   router.get("/files", controller.getListFiles);
   router.get("/files/:name", controller.download);
